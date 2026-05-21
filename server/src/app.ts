7157e978da9272ae9c08f170a089f5a928459cc7
sammern <samreactdev@gmail.com>
2026-05-21 16:43:34 +0300

    refactor(app): add auth routes and add  cors and cookie-parser middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,26 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import noteRoutes from "./routes/note.routes.js";
+import authRoutes from "./routes/auth.route.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import { config } from "./config/env.js";
 
 const app = express();
 
 // Middleware
+app.use(
+  cors({
+    origin: config.clientURL,
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
 // Error handling
