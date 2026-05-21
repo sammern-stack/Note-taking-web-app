a3a77a59280cb56a4d4425df3ba50ff70efa5b85
sammern <samreactdev@gmail.com>
2026-05-21 09:21:06 +0300

    feat(server): use connectDB function in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 import { config } from "./config/env.js";
 
 const start = async (): Promise<void> => {
+  await connectDB();
   app.listen(config.port, () =>
     console.log(`Server is running on port ${config.port}`),
   );
