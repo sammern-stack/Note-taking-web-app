ff97fb42bd1e4e0ce8cb811a98c7c155d5590e62
sammern <samreactdev@gmail.com>
2026-05-21 12:59:27 +0300

    refactor(env): added auth env vars

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,4 +1,13 @@
-const requireEnvVars = ["PORT", "MONGODB_URI"] as const;
+const requireEnvVars = [
+  "PORT",
+  "NODE_ENV",
+  "CLIENT_URL",
+  "MONGODB_URI",
+  "ACCESS_TOKEN_SECRET",
+  "REFRESH_TOKEN_SECRET",
+  "ACCESS_TOKEN_EXPIRY",
+  "REFRESH_TOKEN_EXPIRY",
+] as const;
 
 requireEnvVars.forEach((key) => {
   if (!process.env[key])
@@ -7,5 +16,11 @@ requireEnvVars.forEach((key) => {
 
 export const config = {
   port: Number(process.env.PORT) || 3000,
-  mongodbURI: process.env.MONGODB_URI! 
+  isProduction: process.env.NODE_ENV === "production",
+  clientURL: process.env.CLIENT_URL!,
+  mongodbURI: process.env.MONGODB_URI!,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET!,
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET!,
+  accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY!,
+  refreshTokenExpiry: process.env.REFRESH_TOKEN_EXPIRY!,
 };
