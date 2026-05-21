affeb2cf71258994e89d74a18723717b710d5775
sammern <samreactdev@gmail.com>
2026-05-21 08:05:11 +0300

    feat(app): use errorHandler in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+// Error handling
+app.use(errorHandler);
 
 export default app;
