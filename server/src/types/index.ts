e360a873a122f72387b57dcb4efe1c76926c757e
sammern <samreactdev@gmail.com>
2026-05-21 11:35:15 +0300

    feat(types): added controllers request types

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,3 +1,5 @@
+import type { Response } from "express";
+
 // Models
 export interface INote {
   title: string;
@@ -9,3 +11,11 @@ export interface INote {
 }
 
 export type TNewNote = Omit<INote, "createdAt" | "updatedAt">;
+
+// Controller Requests types
+export interface INoteParams {
+  id: string;
+}
+
+export type ICreateNoteBody = TNewNote;
+export type IUpdateNoteBody = Partial<TNewNote>;
