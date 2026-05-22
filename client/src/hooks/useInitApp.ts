778123b0c2bc898044912d3e4eab7319337fee24
sammern <samreactdev@gmail.com>
2026-05-22 19:29:53 +0300

    feat(app): use theme in app and app hook

--- client/src/hooks/useInitApp.ts
+++ client/src/hooks/useInitApp.ts
@@ -1,13 +1,19 @@
 import { useEffect } from "react";
-import { useAuthStore } from "../stores";
+import { useAuthStore, useThemeStore } from "../stores";
 
 export const useInitApp = () => {
   const { initializeAuth, status } = useAuthStore();
+  const theme = useThemeStore((s) => s.theme);
 
   // Log in user when page mounts
   useEffect(() => {
     initializeAuth();
   }, [initializeAuth]);
+
+  // Set theme from localStorage
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
 
   const isAppLoading: boolean = status === "idle" || status === "loading";
 
--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,12 +1,14 @@
-import { useAuthStore } from "../../stores";
+import { useAuthStore, useThemeStore } from "../../stores";
 
 const HomePage = () => {
   const logout = useAuthStore((s) => s.logout);
+  const toggleTheme = useThemeStore((s) => s.toggleTheme);
 
   return (
     <div>
       <h1>Home</h1>
       <button onClick={() => logout()}>Log out</button>
+      <button onClick={toggleTheme}>Toggle Theme</button>
     </div>
   );
 };
