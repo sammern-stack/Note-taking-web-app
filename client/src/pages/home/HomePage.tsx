a8184530bb4b7e5a9add8129bf1aae374f29c6a0
sammern <samreactdev@gmail.com>
2026-05-22 17:08:26 +0300

    dev: added temporally a logout button

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,7 +1,12 @@
+import { useAuthStore } from "../../stores";
+
 const HomePage = () => {
+  const logout = useAuthStore((s) => s.logout);
+
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={() => logout()}>Log out</button>
     </div>
   );
 };
