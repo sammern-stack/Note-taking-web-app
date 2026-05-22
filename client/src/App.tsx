933b08a7c24cfcaa2ca1fac747b046266dab5077
sammern <samreactdev@gmail.com>
2026-05-22 14:35:30 +0300

    feat(app): wire up routes and auth initialization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/auth/LoginPage";
+import HomePage from "./pages/home/HomePage";
+import { ProtectedRoute } from "./components/shared/ProtectedRoute";
+import { useEffect } from "react";
+import { useAuthStore } from "./stores/useAuthStore";
+
 function App() {
- return (
-    <>
-      <h1>Home page</h1>
-    </>
-  )
+  const { initializeAuth, status } = useAuthStore();
+
+  useEffect(() => {
+    initializeAuth();
+  }, [initializeAuth]);
+
+  if (status === "idle" || status === "loading") {
+    return <div className="app-loader">Loading...</div>;
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/register" element={<RegisterPage />} /> */}
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/notes" element={<NotesPage />} /> */}
+          {/* <Route path="/profile" element={<ProfilePage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
