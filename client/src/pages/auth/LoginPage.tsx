1c148ff2c1904f18bf5b6dacb82ca0035c680255
sammern <samreactdev@gmail.com>
2026-05-22 14:34:37 +0300

    feat(auth): add login, register, forgot and reset password pages

---
+++ client/src/pages/auth/LoginPage.tsx
@@ -0,0 +1,46 @@
+import {
+  FormHeader,
+  FormFooter,
+  AuthForm,
+  FormField,
+} from "../../components/features/auth";
+import { useLogin } from "../../hooks/auth/useLogin";
+import "./Auth.scss";
+
+export const LoginPage = () => {
+  const formik = useLogin();
+
+  return (
+    <div className="auth">
+      <div className="auth__page-logo">
+        <img src="../../assets/images/logo.svg" alt="site-logo" />
+
+        <FormHeader
+          title="Welcome to Note"
+          subtitle="Please log in to continue"
+        />
+
+        <AuthForm
+          formik={formik}
+          submitLabel="Login"
+          submittingLabel="Logging in..."
+        >
+          <FormField
+            fieldName="email"
+            errorName="email"
+            type="email"
+            label="Email address"
+          />
+          <FormField
+            fieldName="password"
+            errorName="password"
+            type="password"
+            label="Password"
+          />
+        </AuthForm>
+
+        <FormFooter label="No account yet?" nav="Sign up" link="/signup" />
+      </div>
+    </div>
+  );
+};
