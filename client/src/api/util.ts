35a468403300e4a06f2ff9cd2aafaa599b445711
sammern <samreactdev@gmail.com>
2026-05-23 07:57:12 +0300

    refactor: utility added type for readability

--- client/src/api/util.ts
+++ client/src/api/util.ts
@@ -2,10 +2,9 @@ import type { AxiosError } from "axios";
 import type { ApiResponse, ApiResult } from "../types";
 
 type TFetchApiFn<T> = () => Promise<{ data: ApiResponse<T> }>;
+type TFetchRes<T> = Promise<ApiResult<T>>;
 
-export const fetchApi = async <T>(
-  fn: TFetchApiFn<T>,
-): Promise<ApiResult<T>> => {
+export const fetchApi = async <T>(fn: TFetchApiFn<T>): TFetchRes<T> => {
   try {
     const { data } = await fn();
     return { ok: true, data: data.data };
