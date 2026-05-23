43d5359bb2b0de258b6224a8f2d99d4e4c01692c
sammern <samreactdev@gmail.com>
2026-05-23 14:01:30 +0300

    feat: added a button component

---
+++ client/src/components/shared/Button/Button.tsx
@@ -0,0 +1,13 @@
+import type React from "react";
+import "./Button.scss";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: "primary" | "warning" | "cancel";
+}
+
+export const Button = ({ children, variant, ...props }: ButtonProps) => (
+  <button className={`button button--${variant ?? "primary"}`} {...props}>
+    {children}
+  </button>
+);
--- client/src/components/shared/index.ts
+++ client/src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { ProtectedRoute } from "./ProtectedRoute";
 export { AppLoader } from "./AppLoader";
 export { SiteLogo } from "./SiteLogo/SiteLogo";
 export { Title } from "./Title/Title";
+export { Button } from "./Button/Button";
