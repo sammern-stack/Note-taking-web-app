610ec9765a13029fece48161c3cf2bcb505e42b9
sammern <samreactdev@gmail.com>
2026-05-24 12:17:28 +0300

    refactor: create template code and a barrel file

---
+++ client/src/hooks/auth/index.ts
@@ -0,0 +1,4 @@
+export { useForgotPassword } from "./useForgotPassword";
+export { useLogin } from "./useLogin";
+export { useRegister } from "./useRegister";
+export { useResetPassword } from "./useResetPassword";
---
+++ client/src/hooks/auth/useForgotPassword.ts
@@ -0,0 +1,12 @@
+import * as Yup from "yup";
+import type { TOnSubmit } from "../../types";
+
+export const useForgotPassword = () => {
+  const initialValues = {};
+  const validationSchema = Yup.object({});
+
+  type TValues = typeof initialValues;
+  const onSubmit: TOnSubmit<TValues> = async (values, { setFieldError }) => {};
+
+  return { initialValues, validationSchema, onSubmit };
+};
