bc1f9e310329f4481c8b3b5ff8957c863228eb67
sammern <samreactdev@gmail.com>
2026-05-24 06:40:45 +0300

    refactor: update button component with new styles and variant options

--- client/src/components/shared/Button/Button.tsx
+++ client/src/components/shared/Button/Button.tsx
@@ -3,7 +3,7 @@ import "./Button.scss";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  variant?: "primary" | "warning" | "cancel";
+  variant?: "primary" | "secondary" | "warning" | "cancel";
 }
 
 export const Button = ({ children, variant, ...props }: ButtonProps) => (
