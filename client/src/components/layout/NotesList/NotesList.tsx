70b5a5ef5af726c7d7132f44458badd6b53e292b
sammern <samreactdev@gmail.com>
2026-05-25 17:16:06 +0300

    feat: create a notes list component

---
+++ client/src/components/layout/NotesList/NotesList.tsx
@@ -0,0 +1,34 @@
+import { useNotesStore } from "../../../stores";
+import { formatDate } from "../../../utils/formatters";
+import "./NotesList.scss";
+
+export const NotesList = () => {
+  const notes = useNotesStore((s) => s.notes);
+
+  return (
+    <div className="home__notes-list notes">
+      {notes.map((note) => (
+        <div className="notes__note">
+          <span className="notes__note-title">{note.title}</span>
+
+          <div className="notes__note-tags">
+            {note.tags.map((tag) => (
+              <div className="notes__note-tag">{tag}</div>
+            ))}
+          </div>
+
+          <div className="notes__note-dates">
+            <div className="notes__note-created">
+              {formatDate(note.createdAt)}
+            </div>
+
+            <div className="notes__note-updated">
+              <span>Updated: </span>
+              {formatDate(note.updatedAt)}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
--- client/src/components/layout/index.ts
+++ client/src/components/layout/index.ts
@@ -1,2 +1,3 @@
 export { AuthWrapper } from "./AuthWrapper";
 export { SideBar } from "./SideBar/SideBar";
+export { NotesList } from "./NotesList/NotesList";
