69f5f91745aad23d60cde415ea89714a81c5a77e
sammern <samreactdev@gmail.com>
2026-05-25 11:35:36 +0300

    refactor(title): added custom className and new styles

--- client/src/components/shared/Title/Title.tsx
+++ client/src/components/shared/Title/Title.tsx
@@ -4,8 +4,9 @@ import "./Title.scss";
 interface TitleProps {
   children: React.ReactNode;
   size: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  className?: string;
 }
 
-export const Title = ({ children, size }: TitleProps) => (
-  <div className={`title-${size}`}>{children}</div>
+export const Title = ({ children, size, className }: TitleProps) => (
+  <div className={`title-${size} ${className}`}>{children}</div>
 );
