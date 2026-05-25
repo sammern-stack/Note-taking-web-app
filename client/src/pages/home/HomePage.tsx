af238ab164698179a103ab31ff1bce96b170359c
sammern <samreactdev@gmail.com>
2026-05-25 13:45:19 +0300

    refactor(home): add basic content with correct layout

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,14 +1,29 @@
+import { SideBar } from "../../components/layout";
 import { useAuthStore, useThemeStore } from "../../stores";
+import "./HomePage.scss";
 
 const HomePage = () => {
   const logout = useAuthStore((s) => s.logout);
   const toggleTheme = useThemeStore((s) => s.toggleTheme);
 
   return (
-    <div>
-      <h1>Home</h1>
-      <button onClick={() => logout()}>Log out</button>
-      <button onClick={toggleTheme}>Toggle Theme</button>
+    <div className="home">
+      <SideBar />
+
+      <div className="home__content">
+        <div className="home__header">Header</div>
+
+        <div className="home__body">
+          <div className="home__notes-list">Note List</div>
+          <div className="home__note-editor">
+            Editor
+            <h1>Home</h1>
+            <button onClick={() => logout()}>Log out</button>
+            <button onClick={toggleTheme}>Toggle Theme</button>
+          </div>
+          <div className="home__note-actions">Actions</div>
+        </div>
+      </div>
     </div>
   );
 };
