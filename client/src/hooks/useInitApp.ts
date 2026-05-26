9e8b4aa93eb79d217b5fa267a91f7615787bde1d
sammern <samreactdev@gmail.com>
2026-05-26 13:39:09 +0300

    refactor app so loads active note content on load and when switch active note

--- client/src/hooks/useInitApp.ts
+++ client/src/hooks/useInitApp.ts
@@ -6,6 +6,9 @@ export const useInitApp = () => {
   const theme = useThemeStore((s) => s.theme);
   const setNotes = useNotesStore((s) => s.setNotes);
 
+  const noteSelected = useNotesStore((s) => s.noteSelected);
+  const setActiveNote = useNotesStore((s) => s.setActiveNote);
+
   // Log in user when page mounts
   useEffect(() => {
     initializeAuth();
@@ -17,8 +20,9 @@ export const useInitApp = () => {
   }, [theme]);
 
   useEffect(() => {
-    if (user) setNotes();
-  }, [user]);
+    if (user) setNotes(); // Load notes when user logs in
+    if (user && noteSelected !== "") setActiveNote(noteSelected);
+  }, [user, noteSelected]);
 
   const isAppLoading: boolean = status === "idle" || status === "loading";
 
