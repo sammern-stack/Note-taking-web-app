58a74903bf9727ba2d47372bbec3635f1bd1ba0f
sammern <samreactdev@gmail.com>
2026-05-26 15:33:48 +0300

    refactor button component with custom className prop and new styles

--- client/src/components/shared/Button/Button.tsx
+++ client/src/components/shared/Button/Button.tsx
@@ -4,10 +4,19 @@ import "./Button.scss";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   variant?: "primary" | "secondary" | "warning" | "cancel";
+  className?: string;
 }
 
-export const Button = ({ children, variant, ...props }: ButtonProps) => (
-  <button className={`button button--${variant ?? "primary"}`} {...props}>
+export const Button = ({
+  children,
+  variant,
+  className,
+  ...props
+}: ButtonProps) => (
+  <button
+    className={`button button--${variant ?? "primary"} ${className}`}
+    {...props}
+  >
     {children}
   </button>
 );
